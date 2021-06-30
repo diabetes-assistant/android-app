e1a4c54a7bec416764e08da09278e240391e3d3b
riberg <richard_bergholz@hotmail.com>
2021-06-30 16:26:21 +0200

    Änderungen gem. Pipeline u.a. kleinere Änderungen

--- app/src/main/java/com/github/diabetesassistant/auth/RegisterActivity.kt
+++ app/src/main/java/com/github/diabetesassistant/auth/RegisterActivity.kt
@@ -1,12 +1,16 @@
 package com.github.diabetesassistant.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.diabetesassistant.R
+import androidx.appcompat.app.AppCompatActivity
+import com.github.diabetesassistant.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
