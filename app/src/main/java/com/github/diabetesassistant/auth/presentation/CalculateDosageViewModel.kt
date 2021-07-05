644602e5c559e13e7c77b441526a770c206acbea
riberg <richard_bergholz@hotmail.com>
2021-07-05 20:39:25 +0200

    CalculateDosageViewModel.kt ausgearbeitet, dabei an LoginActivity.kt orientiert.

--- app/src/main/java/com/github/diabetesassistant/auth/presentation/CalculateDosageViewModel.kt
+++ app/src/main/java/com/github/diabetesassistant/auth/presentation/CalculateDosageViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 class CalculateDosageViewModel : ViewModel() {
     val glucoseLevel=MutableLiveData("")
     val carbohydrateAmount=MutableLiveData("")
+    val insulinDosageRecommended=MutableLiveData("")
 
     // TODO Hier auch noch klären, ob schon Blutzuckerober- und -untergrenzen berücksichtigt werden sollen
     // TODO Gibt es noch andere Eingaben, die invalid sind?
