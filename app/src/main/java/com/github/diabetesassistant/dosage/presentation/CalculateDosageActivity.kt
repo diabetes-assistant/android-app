4031bdf872b4c1ab8569f8b5e8c93491edc1bdfa
riberg <richard_bergholz@hotmail.com>
2021-07-09 11:55:03 +0200

    Algorithmus zur Insulin-Dosisberechnung im CalculateDosageViewModel.kt implementiert. Ausgabe allerdings bisher nur im Logcat.

--- app/src/main/java/com/github/diabetesassistant/dosage/presentation/CalculateDosageActivity.kt
+++ app/src/main/java/com/github/diabetesassistant/dosage/presentation/CalculateDosageActivity.kt
@@ -1,6 +1,7 @@
 package com.github.diabetesassistant.dosage.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
@@ -12,6 +13,7 @@ class CalculateDosageActivity : AppCompatActivity() {
 
     private lateinit var calculateDosageViewModel: CalculateDosageViewModel
     private lateinit var binding: ActivityCalculateDosageBinding
+    private val TAG ="CalculateDosageActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +26,8 @@ class CalculateDosageActivity : AppCompatActivity() {
             .doOnTextChanged(this::setAmountCarbohydratesState)
         binding.calculateDosageSubmitButton.setOnClickListener(this::handleSubmit)
         // TODO muss man für calculateDosage das Binding in die andere Richtung, vom ViewModel ausgehend implementieren?
-        // binding.calculateDosageDosageResult ...
+        // TODO bzw. reicht es, wenn das entsprechende Feld im ViewModel über die Methode handleSubmit verändert wird?
+        binding.calculateDosageResult.doOnTextChanged(this::setInsulinDosageState)
     }
 
     @Suppress("UNUSED_PARAMETER")
@@ -38,10 +41,14 @@ class CalculateDosageActivity : AppCompatActivity() {
             errorSnackbar.show()
         } else {
             // TODO hier muss dann die Berechnung der Insulindosis initiiert werden
+            // TODO Die Berechnung selber habe ich ins ViewModel verschoben
+            calculateDosageViewModel.calculateInsulinDosage()
+
         }
     }
 
-    // TODO was sind hier genau die Parameter dieser Funktionen, v.a. die Integer a-c?
+    // TODO wann immer die Patient:in neue Werte eingibt muss der Wert für die empfohlene Insulindosis zunächst
+    // TODO gelöscht werden und darf erst nach Betätigung des Submit-Buttons wieder neu berechnet erscheinen
     @Suppress("UNUSED_PARAMETER")
     private fun setGlucoseLevelState(chars: CharSequence?, a: Int, b: Int, c: Int) {
         this.calculateDosageViewModel.glucoseLevel.value = chars.toString()
@@ -50,5 +57,10 @@ class CalculateDosageActivity : AppCompatActivity() {
     @Suppress("UNUSED_PARAMETER")
     private fun setAmountCarbohydratesState(chars: CharSequence?, a: Int, b: Int, c: Int) {
         this.calculateDosageViewModel.carbohydrateAmount.value = chars.toString()
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun setInsulinDosageState(chars: CharSequence?, a: Int, b: Int, c: Int) {
+        this.calculateDosageViewModel.insulinDosageRecommended.value=chars.toString()
     }
 }
--- app/src/main/java/com/github/diabetesassistant/dosage/presentation/CalculateDosageViewModel.kt
+++ app/src/main/java/com/github/diabetesassistant/dosage/presentation/CalculateDosageViewModel.kt
@@ -1,12 +1,38 @@
 package com.github.diabetesassistant.dosage.presentation
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.math.roundToInt
 
 class CalculateDosageViewModel : ViewModel() {
+    val TAG: String = "CalculateDosageViewModel"
+
+    // Felder, die auch im UI sichtbar sind
+    // TODO wieso werden hier val (final) und nicht var verwendet?
     val glucoseLevel = MutableLiveData("")
     val carbohydrateAmount = MutableLiveData("")
     val insulinDosageRecommended = MutableLiveData("")
+
+    // Durch die Ärzt:in voreingestellte Felder
+    // Kohlenhydrateinheit
+    // The amount of insulin [IE] which is needed to normalize the blood glucose level
+    // per intake of 1 carbohydrate unit (10 g of carbohydrates)
+    // Will finally be entered by the doctor but is arbitrarily set here to 0.5 for testing purposes
+    // TODO sollte durch Arzt gesetzt werden
+    val keFactor: Float = 0.5F
+
+    // Größe eines Korrekturintervalls in [mg/dl]
+    // TODO sollte durch Ärzt:in gesetzt werden, hier aber zu Testzwecken erstmal auf 20 mg/dl gesetzt
+    val glucoseLevelCorrectionInterval: Int = 20
+
+    // Korrektureinheiten: Insulineinheiten [IE], die pro Korrekturschritt erforderlich sind
+    // TODO sollte durch Ärzt:in gesetzt werden, hier aber erstmal auf 2 IE gesetzt
+    val glucoseLevelCorrectionDosage: Int = 2
+
+    // Oberer Normwert für den Blutzuckerspiegel, i.d.R. 120 mg/dl
+    // TODO dieser obere Blutzuckernormwert sollte durch Ärzt:in änderbar sein, hier aber erstmal auf 120 mg/dl gesetzt
+    val glucoseLevelUpperLimit: Int = 120
 
     // TODO Hier auch noch klären, ob schon Blutzuckerober- und -untergrenzen berücksichtigt werden sollen
     // TODO Gibt es noch andere Eingaben, die invalid sind?
@@ -14,5 +40,19 @@ class CalculateDosageViewModel : ViewModel() {
         return this.glucoseLevel.value
             .isNullOrBlank() || this.carbohydrateAmount
             .value.isNullOrBlank()
+    }
+
+    fun calculateInsulinDosage() {
+        var insulinDosageRecommended: Int
+        var carbohydrateAmountInt: Int = this.carbohydrateAmount.value.toString().toInt()
+        var insulinStandardDosage: Int = (carbohydrateAmountInt * this.keFactor).roundToInt()
+        var glucoseLevelDiscrepancy: Int = this.glucoseLevel.value.toString().toInt() -
+                this.glucoseLevelUpperLimit
+        // TODO ceil nutzen, damit aufgerundet wird
+        var correctionStepsInt: Int=(glucoseLevelDiscrepancy/this.glucoseLevelCorrectionInterval)
+        insulinDosageRecommended = insulinStandardDosage+(correctionStepsInt*this.glucoseLevelCorrectionDosage)
+        this.insulinDosageRecommended.value=insulinDosageRecommended.toString()
+
+            Log.v(TAG, "insulinDosageRecommended= " + this.insulinDosageRecommended.value.toString())
     }
 }
