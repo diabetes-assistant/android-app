651965381c09bf62faffa254ecb1803f0ef57fb2
riberg <richard_bergholz@hotmail.com>
2021-07-09 12:38:34 +0200

    Pipeline-Warnungen abgearbeitet

--- app/src/main/java/com/github/diabetesassistant/dosage/presentation/CalculateDosageViewModel.kt
+++ app/src/main/java/com/github/diabetesassistant/dosage/presentation/CalculateDosageViewModel.kt
@@ -20,19 +20,19 @@ class CalculateDosageViewModel : ViewModel() {
     // per intake of 1 carbohydrate unit (10 g of carbohydrates)
     // Will finally be entered by the doctor but is arbitrarily set here to 0.5 for testing purposes
     // TODO sollte durch Arzt gesetzt werden
-    val ke_Factor: Float = 0.5F
+    val keFactor: Float = 0.5F
 
     // Größe eines Korrekturintervalls in [mg/dl]
     // TODO sollte durch Ärzt:in gesetzt werden, hier aber zu Testzwecken erstmal auf 20 mg/dl gesetzt
-    val glucose_Level_Correction_Interval: Int = 20
+    val glucoseLevelCorrectionInterval: Int = 20
 
     // Korrektureinheiten: Insulineinheiten [IE], die pro Korrekturschritt erforderlich sind
     // TODO sollte durch Ärzt:in gesetzt werden, hier aber erstmal auf 2 IE gesetzt
-    val glucose_Level_Correction_Dosage: Int = 2
+    val glucoseLevelCorrectionDosage: Int = 2
 
     // Oberer Normwert für den Blutzuckerspiegel, i.d.R. 120 mg/dl
     // TODO dieser obere Blutzuckernormwert sollte durch Ärzt:in änderbar sein, hier aber erstmal auf 120 mg/dl gesetzt
-    val glucose_Level_Upper_Limit: Int = 120
+    val glucoseLevelUpperLimit: Int = 120
 
     // TODO Hier noch klären, ob schon Blutzuckerober- und -untergrenzen berücksichtigt werden sollen
     // TODO Gibt es noch andere Eingaben, die invalid sind?
@@ -44,14 +44,14 @@ class CalculateDosageViewModel : ViewModel() {
 
     fun calculateInsulinDosage() {
         val carbohydrateAmountInt: Int = this.carbohydrateAmount.value.toString().toInt()
-        val insulinStandardDosage: Int = (carbohydrateAmountInt * this.ke_Factor).roundToInt()
+        val insulinStandardDosage: Int = (carbohydrateAmountInt * this.keFactor).roundToInt()
         val glucoseLevelDiscrepancy: Int =
-            this.glucoseLevel.value.toString().toInt() - this.glucose_Level_Upper_Limit
+            this.glucoseLevel.value.toString().toInt() - this.glucoseLevelUpperLimit
         // TODO ceil nutzen, damit aufgerundet wird
         val correctionStepsInt: Int =
-            (glucoseLevelDiscrepancy / this.glucose_Level_Correction_Interval)
+            (glucoseLevelDiscrepancy / this.glucoseLevelCorrectionInterval)
         val insulinDosageRecommended =
-            insulinStandardDosage + (correctionStepsInt * this.glucose_Level_Correction_Dosage)
+            insulinStandardDosage + (correctionStepsInt * this.glucoseLevelCorrectionDosage)
         this.insulinDosageRecommended.value = insulinDosageRecommended.toString()
 
         Log.v(tag, "insulinDosageRecommended= " + this.insulinDosageRecommended.value.toString())
