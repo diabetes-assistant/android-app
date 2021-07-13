f33ff4e402220ccea4746b9aa3be6a863cbd53aa
riberg <richard_bergholz@hotmail.com>
2021-07-13 12:13:44 +0200

    ShowDiaryActivity.kt etc. angelegt, aber noch nicht lauffähig.

---
+++ app/src/main/java/com/github/diabetesassistant/dosage/presentation/Data.kt
@@ -0,0 +1,3 @@
+package com.github.diabetesassistant.dosage.presentation
+
+class Data(var id: Int, var date: String, var time: String, var bloodGlucose: Int)
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+
     }
 }
 
@@ -14,6 +15,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven ("https://jitpack.io")
     }
 }
 
