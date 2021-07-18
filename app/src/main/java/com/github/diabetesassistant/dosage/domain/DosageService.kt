b17996238e2223ded03c987a50f5b9fd4f7060bc
Daniel Schruhl <daniel.schruhl@gropyus.com>
2021-07-18 17:29:44 +0200

    Add failing test

--- app/src/main/java/com/github/diabetesassistant/dosage/domain/DosageService.kt
+++ app/src/main/java/com/github/diabetesassistant/dosage/domain/DosageService.kt
@@ -3,4 +3,12 @@ package com.github.diabetesassistant.dosage.domain
 import com.github.diabetesassistant.dosage.data.DosageClient
 
 class DosageService(private val client: DosageClient) {
+    fun calculateDosage(
+        userId: String,
+        email: String,
+        glucoseLevel: Int,
+        carbohydrateUnits: Int
+    ): Result<Int> {
+        return Result.failure(Exception())
+    }
 }
