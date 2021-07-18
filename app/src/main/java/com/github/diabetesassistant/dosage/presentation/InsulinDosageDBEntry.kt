91f276cecb01fd555f00aeab51e8132e3d96cf1b
riberg <richard_bergholz@hotmail.com>
2021-07-18 11:27:36 +0200

    Klasse InsulinDosageDBEntry.kt hinzugefügt für die Ergebnisse der Insulin-Berechnungen. Und Pipeline-Warnungen abgearbeitet.

---
+++ app/src/main/java/com/github/diabetesassistant/dosage/presentation/InsulinDosageDBEntry.kt
@@ -0,0 +1,5 @@
+package com.github.diabetesassistant.dosage.presentation
+
+import java.time.LocalDateTime
+
+class InsulinDosageDBEntry(var id: Int, var dateTime: LocalDateTime, var insulinDosage: Int)
