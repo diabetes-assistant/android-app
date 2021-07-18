75ec6c0ce457f3575dd6007696d7e50f5e499640
riberg <richard_bergholz@hotmail.com>
2021-07-18 11:06:54 +0200

    ShowDiaryViewModel.kt: Sortierung der Daten nach Datum funktioniert, graphische Darstellung ist jetzt auch sortiert.

--- app/src/main/java/com/github/diabetesassistant/dosage/presentation/GlucoseLevelDBEntry2.kt
+++
@@ -1,5 +0,0 @@
-package com.github.diabetesassistant.dosage.presentation
-
-import java.time.LocalDateTime
-
-class GlucoseLevelDBEntry2(var id: Int, var dateTime: LocalDateTime, var time: String, var bloodGlucose: Int)
