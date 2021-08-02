763c3470bb5772f0cac9158ab021854d576fd1e9
riberg <richard_bergholz@hotmail.com>
2021-08-02 12:19:23 +0200

    WarningActivity.kt: Bestätigungs- bzw. resumeButton, mit dem man zurück in die CalculateDosageActivity.kt gelangt, funktioniert jetzt. Evtl. in der WarningActivity.kt noch den eingegeben Blutzuckerwert und ggf. auch die KE anzeigen.

--- app/src/main/java/com/github/diabetesassistant/auth/presentation/WarningActivity.kt
+++ app/src/main/java/com/github/diabetesassistant/auth/presentation/WarningActivity.kt
@@ -1,9 +1,12 @@
 package com.github.diabetesassistant.auth.presentation
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.github.diabetesassistant.R
 import com.github.diabetesassistant.databinding.ActivityWarningBinding
+import com.github.diabetesassistant.dosage.presentation.CalculateDosageActivity
 
 class WarningActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWarningBinding
@@ -14,6 +17,7 @@ class WarningActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding.linearLayout.setBackgroundColor(getColor(R.color.red_warning))
+        binding.resumeButton.setOnClickListener(this::handleResume)
 
         // Herausfinden, ob die Activity wegen zu hohem oder zu niedrigem
         // Blutzuckerspiegel gestartet wurde
@@ -25,5 +29,10 @@ class WarningActivity : AppCompatActivity() {
         } else if (errorType == 2) {
             binding.warningTextView.setText(getString(R.string.calculate_dosage_glucose_level_too_low))
         }
+    }
+
+    private fun handleResume(view: View) {
+        intent = Intent(applicationContext, CalculateDosageActivity::class.java)
+        startActivity(intent)
     }
 }
