cca89e681690eaec754b5d8f861d6137ac547d35
riberg <richard_bergholz@hotmail.com>
2021-08-06 11:18:22 +0200

    static-code-check-Warnungen abgearbeitet.

--- app/src/main/java/com/github/diabetesassistant/dosage/data/DosageClient.kt
+++ app/src/main/java/com/github/diabetesassistant/dosage/data/DosageClient.kt
@@ -3,6 +3,7 @@ package com.github.diabetesassistant.dosage.data
 import com.github.diabetesassistant.auth.data.UserDTO
 
 class DosageClient {
+    @Suppress("UNUSED_PARAMETER")
     fun getInsulinPresets(user: UserDTO): Result<InsulinPresetsDTO> {
         return Result.failure(Exception())
     }
